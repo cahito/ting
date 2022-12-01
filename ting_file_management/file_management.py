253c1fd6bd4da9611937b18e50f7acf94dae775d
cahito <cahito@gmail.com>
2022-12-01 16:16:13 -0300

    Feat - Requisito dois cumprido.

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, encoding="utf-8") as file:
+            arquivo = file.read()
+            lista_arquivo = arquivo.split("\n")
+            return lista_arquivo
+    except Exception:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
