54f1a3ccb2748ca2a07eed79396641324aad3ff4
cahito <cahito@gmail.com>
2022-12-14 18:57:48 -0300

    Feat - Requisito seis cumprido.

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,22 +1,27 @@
+from collections import deque
+
+
 class Queue:
     def __init__(self):
-        self._data = []
-        self.__length = 0
+        self._data = deque()
 
     def __len__(self):
-        return self.__length
+        return len(self._data)
 
-    def __str__(self):
-        return str(self._data)
+    # def __str__(self):
+    #    return str(self._data)
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __repr__(self) -> str:
+        return f"queue({[cada for cada in self._data]})"
 
     def enqueue(self, value):
         self._data.append(value)
-        self.__length += 1
 
     def dequeue(self):
-        retirado = self._data.pop(0)
-        self.__length -= 1
-        return retirado
+        return self._data.popleft()
 
     def search(self, index):
         if index not in range(len(self)):
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,25 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
+def exists_word(word: str, instance: Queue):
+    result = []
+    for arquivo in instance:
+        ocorrencias = [
+            {"linha": idx}
+            for idx, linha in enumerate(arquivo["linhas_do_arquivo"], start=1)
+            if word.lower() in linha.lower()
+        ]
+        if ocorrencias:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": arquivo["nome_do_arquivo"],
+                    "ocorrencias": ocorrencias,
+                }
+            )
+
+    return result
+
+
+def search_by_word(word: str, instance: Queue):
     """Aqui irá sua implementação"""
